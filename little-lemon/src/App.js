8757b88ad2b83f8c364653831930f05dc7d74cb1
Zuza M <z95maciejewska@gmail.com>
2024-03-05 15:05:13 -0500

    import elements

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import Nav from './pages/Nav';
+import Header from './pages/Header';
+import Main from './pages/Main';
+import Footer from './pages/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+    <Nav/>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
   );
 }
 
